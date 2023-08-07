38d37e67715f9a82ef03d87adf7f9c8ec5495323
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-07 17:40:53 +0300

    format

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,8 @@
+import '../globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ClerkProvider } from '@clerk/nextjs';
 import { dark } from '@clerk/themes';
-
-import '../globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
