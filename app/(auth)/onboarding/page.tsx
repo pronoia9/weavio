83c14f136b7992dcf8fa0fba97956e0ae4002cf2
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-07 20:52:22 +0300

    update import path

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,30 +1,32 @@
 import { currentUser } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
 
+import { AccountProfile } from '@/components/forms';
 // import { fetchUser } from '@/lib/actions/user.actions';
-import { AccountProfile } from '@/components';
 
 async function Page() {
   const user = await currentUser();
   if (!user) return null; // to avoid typescript warnings
-  const userInfo = {}; // await fetchUser(user.id);
-  if (userInfo?.onboarded) redirect('/');
-  const userData = {
-    id: user.id,
-    objectId: userInfo?._id,
-    username: userInfo ? userInfo?.username : user.username,
-    name: userInfo ? userInfo?.name : user.firstName ?? '',
-    bio: userInfo ? userInfo?.bio : '',
-    image: userInfo ? userInfo?.image : user.imageUrl,
-  };
+
+  // const userInfo = await fetchUser(user.id);
+  // if (userInfo?.onboarded) redirect('/');
+
+  // const userData = {
+  //   id: user.id,
+  //   objectId: userInfo?._id,
+  //   username: userInfo ? userInfo?.username : user.username,
+  //   name: userInfo ? userInfo?.name : user.firstName ?? '',
+  //   bio: userInfo ? userInfo?.bio : '',
+  //   image: userInfo ? userInfo?.image : user.imageUrl,
+  // };
 
   return (
     <main className='mx-auto flex max-w-3xl flex-col justify-start px-10 py-20'>
       <h1 className='head-text'>Onboarding</h1>
-      <p className='mt-3 text-base-regular text-light-2'>Complete your profile to use Weavio.</p>
+      <p className='mt-3 text-base-regular text-light-2'>Complete your profile now, to use Threds.</p>
 
       <section className='mt-9 bg-dark-2 p-10'>
-        <AccountProfile user={userData} btnTitle='Continue' />
+        <AccountProfile btnTitle='Continue' />
       </section>
     </main>
   );
