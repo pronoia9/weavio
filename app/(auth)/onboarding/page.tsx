e3ddf0846efc69c5b840d1f3e6e221fcbfdca06f
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-07 21:17:56 +0300

    disable await currentUser()

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -5,8 +5,8 @@ import { AccountProfile } from '@/components/forms';
 // import { fetchUser } from '@/lib/actions/user.actions';
 
 async function Page() {
-  const user = await currentUser();
-  if (!user) return null; // to avoid typescript warnings
+  // const user = await currentUser();
+  // if (!user) return null; // to avoid typescript warnings
 
   // const userInfo = await fetchUser(user.id);
   // if (userInfo?.onboarded) redirect('/');
