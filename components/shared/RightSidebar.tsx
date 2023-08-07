976e6918e984148b25b4edb9314170bb43b1c928
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-07 19:04:09 +0300

    init rightsidebar / no func

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,4 +1,20 @@
+import { currentUser } from '@clerk/nextjs';
+
+import { UserCard } from '@/components';
+// import { fetchCommunities } from '@/lib/actions/community.actions';
+// import { fetchUsers } from '@/lib/actions/user.actions';
+
 const RightSidebar = () => {
-  return <div>RightSidebar</div>;
+  return (
+    <section className='custom-scrollbar rightsidebar'>
+      <div className='flex flex-1 flex-col justify-start'>
+        <h3 className='text-heading4-medium text-light-1'>Suggested Communities</h3>
+      </div>
+
+      <div className='flex flex-1 flex-col justify-start'>
+        <h3 className='text-heading4-medium text-light-1'>Similar Minds</h3>
+      </div>
+    </section>
+  );
 };
 export default RightSidebar;
