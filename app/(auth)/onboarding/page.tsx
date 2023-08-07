a8ab309b7fdac2b735c6c3321b6716c64db89aed
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-07 21:49:11 +0300

    change userdata ternary

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -5,20 +5,20 @@ import { AccountProfile } from '@/components/forms';
 // import { fetchUser } from '@/lib/actions/user.actions';
 
 async function Page() {
-  // const user = await currentUser();
-  // if (!user) return null; // to avoid typescript warnings
+  const user = await currentUser();
+  if (!user) return null; // to avoid typescript warnings
 
-  // const userInfo = await fetchUser(user.id);
-  // if (userInfo?.onboarded) redirect('/');
+  const userInfo = {}; // await fetchUser(user.id);
+  if (userInfo?.onboarded) redirect('/');
 
-  // const userData = {
-  //   id: user.id,
-  //   objectId: userInfo?._id,
-  //   username: userInfo ? userInfo?.username : user.username,
-  //   name: userInfo ? userInfo?.name : user.firstName ?? '',
-  //   bio: userInfo ? userInfo?.bio : '',
-  //   image: userInfo ? userInfo?.image : user.imageUrl,
-  // };
+  const userData = {
+    id: user.id,
+    objectId: userInfo?._id,
+    username: userInfo?.username || user.username,
+    name: userInfo?.name || user?.firstName || '',
+    bio: userInfo?.bio || '',
+    image: userInfo?.image || user.imageUrl,
+  };
 
   return (
     <main className='mx-auto flex max-w-3xl flex-col justify-start px-10 py-20'>
@@ -26,7 +26,7 @@ async function Page() {
       <p className='mt-3 text-base-regular text-light-2'>Complete your profile now, to use Threds.</p>
 
       <section className='mt-9 bg-dark-2 p-10'>
-        <AccountProfile btnTitle='Continue' />
+        <AccountProfile user={userData} btnTitle='Continue' />
       </section>
     </main>
   );
