c97b20b775336974601a2077074bf096825f3b9f
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-07 18:52:51 +0300

    format

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -18,7 +18,7 @@ const LeftSidebar = () => {
           const isActive = (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route;
           if (link.route === '/profile') link.route = `${link.route}/${userId}`;
           return (
-            <Link href={link.route} key={link.label} className={`leftsidebar_link ${isActive && 'bg-primary-500 '}`}>
+            <Link href={link.route} key={link.label} className={`leftsidebar_link${isActive ? ' bg-primary-500': ''}`}>
               <Image src={link.imgURL} alt={link.label} width={24} height={24} />
               <p className='text-light-1 max-lg:hidden'>{link.label}</p>
             </Link>
