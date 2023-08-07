cd49cbfa347026b4141455870ff890f156f78faa
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-07 22:25:25 +0300

    format

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -10,7 +10,7 @@ async function Page() {
 
   const userInfo = {}; // await fetchUser(user.id);
   if (userInfo?.onboarded) redirect('/');
-
+  
   const userData = {
     id: user.id,
     objectId: userInfo?._id,
