055cdab018f45a2d8b8a5bffc666769f3f1afdad
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-07 21:00:00 +0300

    update ui components index

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,13 +1,5 @@
-// import { Button, buttonVariants } from './ui/button';
-// import { useFormField, Form, FormItem, FormLabel, FormControl, FormDescription, FormMessage, FormField } from './ui/form';
-// import Input from './ui/input';
-// import { Label } from './ui/label';
-// import Menubar from './ui/menubar';
-// import Select from './ui/select';
-// import Tabs from './ui/tabs';
-// import Textarea from './ui/textarea';
-// import Toast from './ui/toast';
-// import Toaster from './ui/toaster';
+import { Button, buttonVariants } from './button';
+import { useFormField, Form, FormItem, FormLabel, FormControl, FormDescription, FormMessage, FormField } from './form';
+import { Label } from './label';
 
-export // Button, buttonVariants, useFormField, Form, FormItem, FormLabel, FormControl, FormDescription, FormMessage, FormField, Input, Label, Menubar, Select, Tabs, Textarea, Toast, Toaster,
- {};
+export { Button, buttonVariants, useFormField, Form, FormItem, FormLabel, FormControl, FormDescription, FormMessage, FormField, Label };
