97965df7db852a4a4cf9b58214718e8c4792fabd
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-08 19:54:54 +0300

    add init comment form

--- components/forms/Comment.tsx
+++ components/forms/Comment.tsx
@@ -1,4 +1,54 @@
-const Comment = () => {
-  return <div>Comment</div>;
+'use client';
+
+import { z } from 'zod';
+import Image from 'next/image';
+import { useForm } from 'react-hook-form';
+import { usePathname } from 'next/navigation';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import { Form, FormControl, FormField, FormItem, FormLabel, Input, Button } from '@/components/ui';
+
+import { CommentValidation } from '@/lib/validations/thread';
+interface Props {
+  threadId: string;
+  currentUserImg: string;
+  currentUserId: string;
+}
+
+const Comment = ({ threadId, currentUserImg, currentUserId }: Props) => {
+  const pathname = usePathname();
+
+  const form = useForm<z.infer<typeof CommentValidation>>({
+    resolver: zodResolver(CommentValidation),
+    defaultValues: { thread: '' },
+  });
+
+  const onSubmit = async (values: z.infer<typeof CommentValidation>) => {
+  };
+
+  return (
+    <Form {...form}>
+      <form className='comment-form' onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name='thread'
+          render={({ field }) => (
+            <FormItem className='flex w-full items-center gap-3'>
+              <FormLabel>
+                <Image src={currentUserImg} alt='current_user' width={48} height={48} className='rounded-full object-cover' />
+              </FormLabel>
+              <FormControl className='border-none bg-transparent'>
+                <Input type='text' {...field} placeholder='Comment...' className='no-focus text-light-1 outline-none' />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <Button type='submit' className='comment-form_btn'>
+          Reply
+        </Button>
+      </form>
+    </Form>
+  );
 };
+
 export default Comment;
