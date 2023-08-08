31e846073dfa19da3709e91fd8992f40278dcfca
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-08 17:08:09 +0300

    add fetch user action

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -18,13 +18,23 @@ interface Params {
   path: string;
 }
 
+export async function fetchUser(userId: string) {
+  try {
+    connectToDB();
+    return await User.findOne({ id: userId });
+    //   .populate({
+    //   path: 'communities',
+    //   model: Community,
+    // });
+  } catch (error: any) {
+    throw new Error(`Failed to fetch user: ${error.message}`);
+  }
+}
+
 export async function updateUser({ userId, image, name, username, bio, path }: Params): Promise<void> {
   try {
     connectToDB();
-    await User.findOneAndUpdate(
-      { id: userId },
-      { username: username.toLowerCase(), name, bio, image, onboarded: true },
-      { upsert: true });
+    await User.findOneAndUpdate({ id: userId }, { username: username.toLowerCase(), name, bio, image, onboarded: true }, { upsert: true });
     if (path === '/profile/edit') revalidatePath(path);
   } catch (error: any) {
     throw new Error(`Failed to create/update user: ${error.message}`);
