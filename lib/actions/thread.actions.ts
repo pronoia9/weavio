46c6f850c1d1e8cf4199e0ef71e7e78c9d41eef3
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-08 18:10:22 +0300

    add fetch posts action

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -7,6 +7,40 @@ import User from '@/lib/models/user.model';
 import Thread from '@/lib/models/thread.model';
 import Community from '@/lib/models/community.model';
 
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+  connectToDB();
+
+  // Calculate the number of posts to skip based on the page number and page size.
+  const skipAmount = (pageNumber - 1) * pageSize;
+
+  // Create a query to fetch the posts that have no parent (top-level threads) (a thread that is not a comment/reply).
+  const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+    .sort({ createdAt: 'desc' })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({ path: 'author', model: User })
+    .populate({ path: 'community', model: Community })
+    .populate({
+      path: 'children', // Populate the children field
+      populate: {
+        path: 'author', // Populate the author field within children
+        model: User,
+        select: '_id name parentId image', // Select only _id and username fields of the author
+      },
+    });
+
+  // Count the total number of top-level posts (threads) i.e., threads that are not comments.
+  const totalPostsCount = await Thread.countDocuments({
+    parentId: { $in: [null, undefined] },
+  }); // Get the total count of posts
+
+  const posts = await postsQuery.exec();
+
+  const isNext = totalPostsCount > skipAmount + posts.length;
+
+  return { posts, isNext };
+}
+
 interface Params {
   text: string;
   author: string;
