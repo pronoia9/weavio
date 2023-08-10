69192102f0f9aa183710f00a08c2b4626acc961a
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-11 00:53:15 +0300

    add getActivity user activity

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -101,9 +101,7 @@ export async function fetchUsers({
     };
 
     // If the search string is not empty, add the $or operator to match either username or name fields.
-    if (searchString.trim() !== '') {
-      query.$or = [{ username: { $regex: regex } }, { name: { $regex: regex } }];
-    }
+    if (searchString.trim() !== '') query.$or = [{ username: { $regex: regex } }, { name: { $regex: regex } }];
 
     // Define the sort options for the fetched users based on createdAt field and provided sort order.
     const sortOptions = { createdAt: sortBy };
@@ -121,6 +119,35 @@ export async function fetchUsers({
     return { users, isNext };
   } catch (error) {
     console.error('Error fetching users:', error);
+    throw error;
+  }
+}
+
+export async function getActivity(userId: string) {
+  try {
+    connectToDB();
+
+    // Find all threads created by the user
+    const userThreads = await Thread.find({ author: userId });
+
+    // Collect all the child thread ids (replies) from the 'children' field of each user thread
+    const childThreadIds = userThreads.reduce((acc, userThread) => {
+      return acc.concat(userThread.children);
+    }, []);
+
+    // Find and return the child threads (replies) excluding the ones created by the same user
+    const replies = await Thread.find({
+      _id: { $in: childThreadIds },
+      author: { $ne: userId }, // Exclude threads authored by the same user
+    }).populate({
+      path: 'author',
+      model: User,
+      select: 'name image _id',
+    });
+
+    return replies;
+  } catch (error) {
+    console.error('Error fetching replies: ', error);
     throw error;
   }
 }
