139ddb8a02fe757cd7d7d5d1c704bc9262d405ab
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-10 23:09:34 +0300

    add profile tabs

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -23,7 +23,33 @@ const Page = async ({ params }: { params: { id: string } }) => {
         imgUrl={userInfo.image}
         bio={userInfo.bio}
       />
-      <UserProfile />
+
+      {/* To edit clerk account info */}
+      {/* <UserProfile /> */}
+
+      <div className='mt-9'>
+        <Tabs defaultValue='threads' className='w-full'>
+          <TabsList className='tab'>
+            {profileTabs.map((tab) => (
+              <TabsTrigger key={tab.label} value={tab.value} className='tab'>
+                <Image src={tab.icon} alt={tab.label} width={24} height={24} className='object-contain' />
+                <p className='max-sm:hidden'>{tab.label}</p>
+
+                {tab.label === 'Threads' && (
+                  <p className='ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-light-2'>{userInfo.threads.length}</p>
+                )}
+              </TabsTrigger>
+            ))}
+          </TabsList>
+
+          {profileTabs.map((tab) => (
+            <TabsContent key={`content-${tab.label}`} value={tab.value} className='w-full text-light-1'>
+              {/* @ts-ignore */}
+              <ThreadsTab currentUserId={user.id} accountId={userInfo.id} accountType='User' />
+            </TabsContent>
+          ))}
+        </Tabs>
+      </div>
     </section>
   );
 };
