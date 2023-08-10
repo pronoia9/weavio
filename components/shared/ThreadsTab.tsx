bcba740362fe4b3e221a58f21c42053c59958640
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-10 23:20:04 +0300

    reverse threads so newest is first

--- components/shared/ThreadsTab.tsx
+++ components/shared/ThreadsTab.tsx
@@ -47,7 +47,7 @@ async function ThreadsTab({ currentUserId, accountId, accountType }: Props) {
 
   return (
     <section className='mt-9 flex flex-col gap-10'>
-      {result.threads.map((thread) => (
+      {result.threads.reverse().map((thread) => (
         <ThreadCard
           key={thread._id}
           id={thread._id}
