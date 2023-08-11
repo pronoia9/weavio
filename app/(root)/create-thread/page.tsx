b0e2ab6d161c4c4275df5da6fe549b898bd1a76f
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-11 20:35:45 +0300

    format

--- app/(root)/create-thread/page.tsx
+++ app/(root)/create-thread/page.tsx
@@ -7,8 +7,6 @@ import { fetchUser } from '@/lib/actions/user.actions';
 async function Page() {
   const user = await currentUser();
   if (!user) return null;
-
-  // fetch organization list created by user
   const userInfo = await fetchUser(user.id);
   if (!userInfo?.onboarded) redirect('/onboarding');
 
