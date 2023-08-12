683bee6d5dd8b0624b549ae43fffb2d9d745745a
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-08-12 21:45:18 +0300

    add searchbar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,19 +1,18 @@
-import { redirect } from 'next/navigation';
 import { currentUser } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
 
-import ThreadCard from '@/components/cards/ThreadCard';
-import Pagination from '@/components/shared/Pagination';
+import { ThreadCard } from '@/components/cards';
+import { Pagination } from '@/components/shared';
 import { fetchPosts } from '@/lib/actions/thread.actions';
 import { fetchUser } from '@/lib/actions/user.actions';
 
-export default async function Home() {
+async function Home({ searchParams }: { searchParams: { [key: string]: string | undefined } }) {
   const user = await currentUser();
   if (!user) return null;
-
   const userInfo = await fetchUser(user.id);
   if (!userInfo?.onboarded) redirect('/onboarding');
 
-  const result = await fetchPosts(/*searchParams.page ? +searchParams.page : */ 1, 30);
+  const result = await fetchPosts(searchParams.page ? +searchParams.page : 1, 30);
 
   return (
     <>
@@ -23,21 +22,27 @@ export default async function Home() {
         {result.posts.length === 0 ? (
           <p className='no-result'>No threads found</p>
         ) : (
-          result.posts.map((post) => (
-            <ThreadCard
-              key={post._id}
-              id={post._id}
-              currentUserId={user.id}
-              parentId={post.parentId}
-              content={post.text}
-              author={post.author}
-              community={post.community}
-              createdAt={post.createdAt}
-              comments={post.children}
-            />
-          ))
+          <>
+            {result.posts.map((post) => (
+              <ThreadCard
+                key={post._id}
+                id={post._id}
+                currentUserId={user.id}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.children}
+              />
+            ))}
+          </>
         )}
       </section>
+
+      <Pagination path='/' pageNumber={searchParams?.page ? +searchParams.page : 1} isNext={result.isNext} />
     </>
   );
 }
+
+export default Home;
